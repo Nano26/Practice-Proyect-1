e4b69bc885202cef03142304f3fea5961e4c3c92
Nano <mrosso.026@gmail.com>
2023-09-04 11:50:46 -0300

    Adds form logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,41 +1,74 @@
-import React from 'react';
-import './Form.css';
+import React from "react";
+import "./Form.css";
 
 const Form = (props) => {
-    return (
-        <form onSubmit={props.formHandler} className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.formHandler(event);
+  };
+
+  const resetHandler = () => {};
+
+  const inputChangeHandler = (input, value) => {};
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("current-savings", event.target.value);
+            }}
+            type="number"
+            id="current-savings"
+          />
         </p>
-      </form>
-    );
-}
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("yearly-contribution", event.target.value);
+            }}
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("expected-return", event.target.value);
+            }}
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("duration", event.target.value);
+            }}
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
 
 export default Form;
